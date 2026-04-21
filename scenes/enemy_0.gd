e6191d2a1546ed7bfdfab0e828665e4c4c11b48d
pcho <paul.k.cho.dev@gmail.com>
2026-04-20 20:36:06 -0700

    enemy script

--- scenes/enemy_0.gd
+++ scenes/enemy_0.gd
@@ -1,12 +1,17 @@
 extends Area3D
 
-@export var speed: float = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += -transform.basis.z * (speed * delta)
+	
 	pass
+
+
+func _on_area_entered(area: Area3D) -> void:
+	area.queue_free()
+	pass # Replace with function body.
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,9 +1,13 @@
 extends CharacterBody3D
 
-@export var SPEED = 5.0
+@export var Speed = 5.0
+@export var FireCooldown = 0.25
 @export var bullet_basic_scene: PackedScene
 
 @onready var marker = $BulletSpawn
+
+var current_cooldown = 0
+var gun_level = 1
 
 signal bullet_fired(position: Vector3, rotation: float)
 
@@ -13,17 +17,28 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * Speed
+		velocity.z = direction.z * Speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, Speed)
+		velocity.z = move_toward(velocity.z, 0, Speed)
 
 	move_and_slide()
 
 func _process(delta):
 	if Input.is_action_pressed("fire"):
-		var bullet_angle = randf_range(-0.1, 0.1)
-		# print(bullet_angle)
-		bullet_fired.emit(marker.global_position, bullet_angle)
+		if current_cooldown == 0:
+			bullet_fired.emit(marker.global_position, 0)
+			if gun_level >= 2:
+				bullet_fired.emit(marker.global_position, -0.1)
+				bullet_fired.emit(marker.global_position, 0.1)
+			if gun_level >= 3:
+				bullet_fired.emit(marker.global_position, -0.05)
+				bullet_fired.emit(marker.global_position, 0.05)
+				
+			current_cooldown = FireCooldown
+	
+	if current_cooldown > 0:
+		current_cooldown = clamp(current_cooldown - delta, 0, 0.25)
+	
 	return
